fa54a86e33262320c2de2331b88c0571cb697ba6
RxnNode <rxnnode@gmail.com>
2019-11-11 14:23:03 +0800

    Update

--- src/SimpleCardGame/PlayCard.java
+++ src/SimpleCardGame/PlayCard.java
@@ -1,14 +1,12 @@
 package SimpleCardGame;
 
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
 public class PlayCard extends DeckofCard {
 
     public static void GameInitialize(){
         Scanner input;
-        String command;
+        String command = "";
         int playNum = 0;
 
         //Make a new deck of card
@@ -22,18 +20,32 @@ public class PlayCard extends DeckofCard {
         while (true){
             System.out.print("Players:");
             input = new Scanner(System.in);
+            if (!input.hasNextInt()){
+                System.err.println("Please enter number only!");
+            }
             try{
                 command = input.nextLine();
+                if (command.isEmpty()){
+                    System.err.println("Please enter number of players.");
+                }
             } catch (NoSuchElementException e){
                 System.err.println();
                 break;
             }
-            if (!input.hasNextInt()){
-                System.err.println("Please enter number only!");
-            }else if (command.isEmpty()){
-                System.err.println("Please enter number of players.");
-            }
+
+            System.out.println(command +" players join the game!");
+            playNum = Integer.valueOf(command);
+            break;
         }
 
+        //Who win
+        //int[] randCard = new int[playNum];
+        //ArrayList<Integer> randCard = new ArrayList<Integer>();
+        System.out.println("Rule : ♠ > ♥ > ♦ > ♣");
+        for (int i = 0; i < playNum; ++i){
+            System.out.println("Player No." + (i+1) +", your card :"+ Deck.get(i).getSuits() + ", "+ Deck.get(i).getRanks());
+            Card bigestcard = new Card();
+
+        }
     }
 }
